8af184cfd6580a17b7fea9d0e8b167887211f514
sawepeter <sawepeter6@gmail.com>
2024-06-10 04:36:37 +0300

    ui components

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import Link from 'next/link';
 import Nav from './Nav';
+import { Button } from './ui/button';
 
 const Header = () => {
   return (
     <header className='py-8 xl:py-12 text-white bg-pink-50/20'>
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between items-center">
             {/* logo */}
             <Link href="/">
                 <h1 className="text-4xl font-semibold">
                     Sawe<span className="text-accent">.</span>
                 </h1>
             </Link>
-            {/* desktop nav */}
-            <Nav />
+            {/* desktop nav & hire me button*/}
+            <div className="hidden xl:flex items-center gap-8">
+              <Nav />
+              <Link href="/contact">
+                <Button>
+                  Hire Me
+                </Button>
+              </Link>
+            </div>
+           
         </div>
     </header>
   )
