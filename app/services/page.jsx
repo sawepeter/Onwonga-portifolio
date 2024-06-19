dcfc0181d1a11d19b76bfac0108f32d1dbb0015d
sawepeter <sawepeter6@gmail.com>
2024-06-19 07:08:54 +0300

    services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -9,24 +9,28 @@ const services = [
     title: "Web Development",
     description: 
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    href: ""
   },
   {
     num: "02",
     title: "UI/UX Design",
     description: 
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    href: ""
   },
   {
     num: "03",
     title: "Logo Design",
     description: 
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    href: ""
   },
   {
     num: "04",
     title: "Mobile Development",
     description: 
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    href: ""
   },
 ];
 
@@ -35,9 +39,41 @@ import { motion } from "framer-motion";
 const Services = () => {
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-      <div className="container mx-auto">services page</div>
+      <div className="container mx-auto">
+        <motion.div 
+        initial={{opacity: 0}} 
+        animate={{opacity: 1, 
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        }}
+        className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((service, index) => {
+            return (
+              <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
+                {/* top */}
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-extrabold text-outline text-transparent">
+                    {service.num}
+                    </div>
+                    <Link href={service.href}>
+                      <BsArrowDownRight />
+                    </Link>
+                  </div>
+                  {/* title */}
+                  <h2>{service.title}</h2>
+                  {/* description */}
+                  <p>{service.description}</p>
+                  {/* border */}
+                  <div className="border-b border-white/20 w-full"></div>
+                
+
+              </div>
+            )
+          })}
+        </motion.div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Services
