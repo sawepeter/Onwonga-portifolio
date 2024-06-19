3c6e3a4c7a1c55feaa45d53aa8346647f5acf524
sawepeter <sawepeter6@gmail.com>
2024-06-19 07:52:10 +0300

    services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -55,14 +55,19 @@ const Services = () => {
                   <div className="text-5xl font-extrabold text-outline text-transparent">
                     {service.num}
                     </div>
-                    <Link href={service.href}>
-                      <BsArrowDownRight />
+                    <Link href={service.href}
+                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all
+                    duration-500 flex justify-center items-center hover:-rotate-45"
+                    >
+                      <BsArrowDownRight className="text-primary text-3xl" />
                     </Link>
                   </div>
                   {/* title */}
-                  <h2>{service.title}</h2>
+                  <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                    {service.title}
+                  </h2>
                   {/* description */}
-                  <p>{service.description}</p>
+                  <p className="text-white/60">{service.description}</p>
                   {/* border */}
                   <div className="border-b border-white/20 w-full"></div>
                 
